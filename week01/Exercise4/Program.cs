b86f2111f42bab0a63ec995a97de61a55b7cda3b
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-03-03 15:45:25 -0700

    Compleated exersise 4.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+        
+        do {
+            Console.Write("Enter a list of numbers, type 0 when finished.  ");
+            number = int.Parse(Console.ReadLine());
+            
+            if (number != 0) {
+                numbers.Add(number);    
+            }
+            
+        } while (number != 0);
+
+        Console.WriteLine($"Sum: {numbers.Sum()}");
+        Console.WriteLine($"Average: {numbers.Sum() / numbers.Count}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
