354931d8e5f3620f49ab730558d22f4e6050db75
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-03-25 16:59:21 -0600

    Refactored the Youtube video program to use a list of videos.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -2,29 +2,34 @@ using System;
 
 class Program
 {
-
+    
     static void Main(string[] args)
     {
-        Video bugs = new Video("James","Fun with Bugs", 344);
-        Video cars = new Video("Billy Bob", "I have a car", 232);
-        Video turtles = new Video("Timmy", "Checkout this turtle!", 655);
+        List<Video> _videos = new List<Video>();
 
+        Video bugs = new Video("James","Fun with Bugs", 344);
         bugs.addComment(new Comment("Tilly","Great video"));
         bugs.addComment(new Comment("Tabby","Do another one about beatles"));
         bugs.addComment(new Comment("Ratchel","I had a pet bug once"));
+        _videos.Add(bugs);
 
+        Video cars = new Video("Billy Bob", "I have a car", 232);
         cars.addComment(new Comment("SamTheMan","I have a car too!!!"));
         cars.addComment(new Comment("Burgman","You can use duct tape to hold that door on"));
         cars.addComment(new Comment("PapaJo","You should paint your car red."));
         cars.addComment(new Comment("PapaJo","No, you should paint it purple."));
+        _videos.Add(cars);
 
+        Video turtles = new Video("Timmy", "Checkout this turtle!", 655);
         turtles.addComment(new Comment("Shmertal","Neat turtle"));
         turtles.addComment(new Comment("Kilburn","I had a turtle once"));
         turtles.addComment(new Comment("Globster","Why do turtles have shells?"));
+        _videos.Add(turtles);
 
         Console.Clear();
-        bugs.display();
-        cars.display();
-        turtles.display();
+        foreach (Video video in _videos)
+        {
+            video.display();
+        }
     }
 }
