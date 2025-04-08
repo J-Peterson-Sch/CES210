5e5b2a20c2f37e39e50c3571f4ac5d5873b76415
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-04-07 19:52:11 -0600

    Moved the shapes progrma over from my work laptop.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.Clear();
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Orange",5);
+        Rectangle rectangle = new Rectangle("Brown",5,10);
+        Circle circle = new Circle("Purple",14);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"color: {shape.GetColor()}");
+            Console.WriteLine($"area: {shape.GetArea()}");
+            Console.WriteLine("");
+        }
     }
 }
